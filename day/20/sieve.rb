2affee115b4b43f769b0d4d4c54624c5e097af09
Ben Cordes <cordes.ben@gmail.com>
2015-12-21 21:57:47 -0500

    Day 20 solution part 1

---
+++ day/20/sieve.rb
@@ -0,0 +1,40 @@
+require 'prime'
+
+def factors_of(number)
+  primes, powers = number.prime_division.transpose
+  exponents = powers.map{|i| (0..i).to_a}
+  divisors = exponents.shift.product(*exponents).map do |powers|
+    primes.zip(powers).map{|prime, power| prime ** power}.inject(:*)
+  end
+  divisors.sort
+end
+
+def how_many_presents(house_num)
+  factors_of(house_num).reduce(:+) * 10
+end
+
+limit = ARGV.shift.to_i
+puts "Limit: #{limit}"
+house_num = 1
+presents = 10
+
+@linelen = 0
+def print_dot
+  print '.'
+  puts if @linelen == 80
+end
+
+start = Time.now.utc
+lap = start
+puts
+while presents < limit
+  house_num += 1
+  presents = how_many_presents house_num
+  if (house_num % 1000 == 0)
+    finish = Time.now.utc
+    print "\033[1A"
+    puts "%d %.2f %.3f" % [ house_num, finish-start, finish-lap ]
+    lap = finish
+  end
+end
+puts "#{house_num}: #{presents}"
